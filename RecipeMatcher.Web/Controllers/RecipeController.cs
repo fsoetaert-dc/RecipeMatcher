3a6e62de43fa47882fd14651d850a2c22d318c48
Frederik <frederik.soetaert@hotmail.com>
2026-08-24 10:17:52 +0200

    show recipes

--- RecipeMatcher.Web/Controllers/RecipeController.cs
+++ RecipeMatcher.Web/Controllers/RecipeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RecipeMatcher.Web.Models;
 
 namespace RecipeMatcher.Web.Controllers;
 
@@ -6,6 +7,13 @@ public class RecipesController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        var recipes =
+            new List<Recipe>
+            {
+            new() { Id = 1, Name = "Pancakes", PreparationMinutes = 20 },
+            new() { Id = 2, Name = "Tomato Soup", PreparationMinutes = 30 }
+            };
+
+        return View(recipes);
     }
 }
---
+++ RecipeMatcher.Web/Models/Recipe.cs
@@ -0,0 +1,10 @@
+namespace RecipeMatcher.Web.Models;
+
+public class Recipe
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public int PreparationMinutes { get; set; }
+}
